87d8fd1026e461e8dc76da9cd6036fc45b330764
Marlon Fauser <marlon.fauser@gmail.com>
2022-05-18 03:31:21 -0300

    Adding project

--- src/main/kotlin/local/Database.kt
+++ src/main/kotlin/local/Database.kt
@@ -7,8 +7,8 @@ object Database {
     val candlesticks: MutableList<Candlestick> = mutableListOf()
 
     fun MutableList<Candlestick>.findNotClosed(isin: ISIN) = find { it.isin == isin && it.closeTimestamp == null }
-    fun MutableList<Candlestick>.deleteBy(isin: ISIN) = remove(findBy(isin))
-    fun MutableList<Candlestick>.findBy(isin: ISIN) = find { it.isin == isin }
+    fun MutableList<Candlestick>.deleteBy(isin: ISIN) = removeAll(filterBy(isin))
+    private fun MutableList<Candlestick>.filterBy(isin: ISIN) = filter { it.isin == isin }
 
     fun new(candlestick: Candlestick) = candlesticks.add(candlestick)
 }
